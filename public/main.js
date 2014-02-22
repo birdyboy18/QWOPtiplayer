b5bc918cec7adfcad7abf8be8f9eb1ef796af8c2
bhawkes <brandonhawkes@Brandons-MacBook-Pro.local>
2014-02-22 15:37:49 +0000

    doing some html

--- public/main.js
+++ public/main.js
@@ -1,4 +1,9 @@
 $(document).on('ready',function(){
+    
+    
+    
+    
+    /////////////////
 
     var keys = [false,false,false,false];
     
@@ -8,11 +13,13 @@ $(document).on('ready',function(){
     
     //socket.emit('tesco',{keys:keys});
     
-        socket.on('players',function(data){
-            console.log(data);
-        });
+    socket.on('players',function(data){
+        console.log(data);
+    });
     
-        
+    socket.on('democracy',function(data){
+         console.log(data);
+    });
     
     function sendKeyState(){
         socket.emit('keys',keys);
@@ -25,6 +32,39 @@ $(document).on('ready',function(){
             keys[0] = true;
         } else if (event.type == "touchend" || event.type == "mouseup"){
             keys[0] = false;
+        }
+        
+        sendKeyState();
+    });
+    
+    $('#w').on('mousedown mouseup touchstart touchend',function(event){
+        
+        if(event.type == "touchstart" || event.type == "mousedown"){
+            keys[1] = true;
+        } else if (event.type == "touchend" || event.type == "mouseup"){
+            keys[1] = false;
+        }
+        
+        sendKeyState();
+    });
+    
+    $('#o').on('mousedown mouseup touchstart touchend',function(event){
+        
+        if(event.type == "touchstart" || event.type == "mousedown"){
+            keys[2] = true;
+        } else if (event.type == "touchend" || event.type == "mouseup"){
+            keys[2] = false;
+        }
+        
+        sendKeyState();
+    });
+    
+    $('#p').on('mousedown mouseup touchstart touchend',function(event){
+        
+        if(event.type == "touchstart" || event.type == "mousedown"){
+            keys[3] = true;
+        } else if (event.type == "touchend" || event.type == "mouseup"){
+            keys[3] = false;
         }
         
         sendKeyState();
