11257098b66ec8fffc77b502a54df7bb62887f39
seanmtracey <seanmtracey@gmail.com>
2014-02-22 15:33:41 +0000

    repaire

--- app.js
+++ app.js
@@ -18,20 +18,20 @@ var express = require('express'),
 	});
 
 
-	var sp = new SerialPort("/dev/tty.usbmodem1411", { 
+	/*var sp = new SerialPort("/dev/tty.usbmodemfd131", { 
 	    baudrate : 9600,
 	    parser: serialport.parsers.readline("\n")
 	});
 
 	sp.on('open', function(){
 
-		canWrite = true;
+		console.log("open");
 
 		sp.on('data', function(data){
 			console.log(data);
 		});
 
-	});
+	});*/
 
     var playerCount = 0;
 
@@ -52,11 +52,6 @@ var express = require('express'),
             updateKeys();
 		});
 
-		socket.on('keys', function(data){
-			socket.keys = data;
-
-		});
-
 		socket.on('disconnect', function(){
 			delete players[socket.id];
             playerCount--;
@@ -64,6 +59,32 @@ var express = require('express'),
 
 	});
 
+	function updateKeys() {
+        var qCount = 0;
+        var wCount = 0;
+        var oCount = 0;
+        var pCount = 0;
+        
+		for ( var id in players) {
+            
+			var player = players[id];
+            
+            if(player.keys[0]) qCount++;
+            if(player.keys[1]) wCount++;
+            if(player.keys[2]) oCount++;
+            if(player.keys[3]) pCount++;
+                	
+		}
+        
+        game.arduinoKeys[0] = Math.round((qCount / playerCount)) > .5 ? true : false;
+        game.arduinoKeys[1] = Math.round((wCount / playerCount)) > .5 ? true : false;
+        game.arduinoKeys[2] = Math.round((oCount / playerCount)) > .5 ? true : false;
+        game.arduinoKeys[3] = Math.round((pCount / playerCount)) > .5 ? true : false;
+    
+        console.log(playerCount,game.arduinoKeys);
+        
+        
+	}
 
 	function sendToArduino(dasKeys){
 
@@ -91,69 +112,3 @@ var express = require('express'),
 		}
 
 	}
-
-//sendToArduino([true, true, false, true]);
-
-setInterval(function(){
-	sendToArduino();
-}, 1000);
-
-function updateKeys() {
-    var qCount = 0;
-    var wCount = 0;
-    var oCount = 0;
-    var pCount = 0;
-    
-	for ( var id in players) {
-        
-		var player = players[id];
-        
-        if(player.keys[0]) qCount++;
-        if(player.keys[1]) wCount++;
-        if(player.keys[2]) oCount++;
-        if(player.keys[3]) pCount++;
-            	
-	}
-    
-    game.arduinoKeys[0] = Math.round((qCount / playerCount)) > .5 ? true : false;
-    game.arduinoKeys[1] = Math.round((wCount / playerCount)) > .5 ? true : false;
-    game.arduinoKeys[2] = Math.round((oCount / playerCount)) > .5 ? true : false;
-    game.arduinoKeys[3] = Math.round((pCount / playerCount)) > .5 ? true : false;
-
-    console.log(playerCount,game.arduinoKeys);
-    
-    
-}
-
-function sendToArduino(dasKeys){
-
-	var keys = dasKeys || game.arduinoKeys,
-		keyIndexes = ["q", "w", "o", "p"];
-		keyString = "";
-
-	for(var x = 0; x < keys.length; x += 1){
-
-		switch(keys[x]){
-			case true:
-				keyString += keyIndexes[x];
-				break;
-			case false:
-				keyString += "s"
-				break;
-		}
-
-	}
-
-	console.log("Hello");
-
-	if(canWrite){
-		sp.write(keyString);
-	}
-
-}
-
-//sendToArduino([true, true, false, true]);
-
-setInterval(function(){
-	sendToArduino();
-}, 1000);
