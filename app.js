f9e7df4b406c7a3faed0e5ef2869af2c89515854
Paul Bird <paulbird1993@gmail.com>
2014-02-22 11:58:37 +0000

    added var

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 
-	SerialPort = require('serialport').SerialPort;
+	var SerialPort = require('serialport').SerialPort;
 
 	server.listen(3000);
 
 	app.use(express.static(__dirname + "/public"));
 
 	app.get('/',function(req,res){
-		res.send("Hello People");
+		res.send('Hello People');
 	});
 
 
